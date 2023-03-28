9f8867adac660c749a1e6fdb9189c77cf25b9067
Michael Lan <mlan@motorway.co.uk>
2023-03-28 13:11:24 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+
+  import Counter from './lib/Counter.svelte'
+  import Header from './lib/Header.svelte'
+</script>
+
+<main>
+  <Header title="Sieve"/>
+  <h1>Sieve</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Built using:
+  </p>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo vite" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 3em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: 0.3s;
+  }
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #646cff);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let title = 'My App'; // optional prop for the title text
+</script>
+
+<header>
+  <h1>{title}</h1>
+  <nav>
+    <!-- Navigation links would go here -->
+  </nav>
+</header>
+
+<style>
+  /* Add your styling for the header here */
+  header {
+    background-color: #333;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+  }
+  
+  h1 {
+    margin: 0;
+  }
+</style>
